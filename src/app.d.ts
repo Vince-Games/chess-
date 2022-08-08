b33ef5e210032bd4bfc3b3a6dc3d95635ef4e776
Panth <hybrid977@gmail.com>
2022-08-08 20:14:11 +0530

    set-up

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+/// <reference types="@sveltejs/adapter-cloudflare" />
 declare namespace App {
 	// interface Locals {}
-	// interface Platform {}
+	interface Platform {
+		env?: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<never>): void;
+		};
+		caches: CacheStorage & { default: Cache };
+	}
+
 	// interface PrivateEnv {}
 	// interface PublicEnv {}
 	// interface Session {}
